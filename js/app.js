e966159f1cdaa98b4ce186a5e43acc536fc995ca
cannn <ngoccanscitech@gmail.com>
2022-11-08 07:28:32 +0700

    first commit

---
+++ js/app.js
@@ -0,0 +1,31 @@
+$(document).ready(function() {
+    $('.image-slider').slick({
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        infinite: true,
+        arrows: true,
+        // autoplay: true,
+        // autoplaySpeed: 1000,
+        draggable: false,
+        prevArrow:`<button type='button' class='slick-prev slick-arrow'><ion-icon name="arrow-back-outline"></ion-icon></button>`,
+        nextArrow:`<button type='button' class='slick-next slick-arrow'><ion-icon name="arrow-forward-outline"></ion-icon></button>`,
+        dots: true,
+        responsive: [
+            {
+                breakpoint: 1024,
+                settings: {
+                    slidesToShow: 3,
+                }
+            },
+            {
+                breakpoint: 480,
+                settings: {
+                    slidesToShow: 1,
+                    arrows: false,
+                    infinite: false
+                }
+            }
+        ]
+    })
+});
+
